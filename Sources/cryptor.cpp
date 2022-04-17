e58835227dec58580c95f80970f1c842d796ec4d
0xF5T9 <truyenhaunhan@gmail.com>
2022-04-17 21:59:00 +0700

    rework..

--- Sources/cryptor.cpp
+++ Sources/cryptor.cpp
@@ -1,5 +1,7 @@
 ﻿#include <iostream>
+#include <string>
 #include <fstream>
+#include <Windows.h>
 #include "../Sources/Headers/cryptor.h"
 #include "../Sources/Headers/animation.h"
 
@@ -8,19 +10,27 @@ animation AnimationCryptorObj;
 
 /*	Định nghĩa các hàm class 'cryptor' (Define 'cryptor' class functions)	*/
 void cryptor::Encrypt(std::string filename, int pin) {
+	DWORD dw1;
+	DWORD dw2;
+	double dw3;
 	char i;
-	std::fstream InputFile, TemporyFile;	
-	InputFile.open(filename, std::fstream::in);
+	dw1 = GetTickCount64();
+	std::string OriginalFileName = filename;
+	std::string TempData;
+	std::ifstream InputFile(filename, std::ios::binary);
+	std::ofstream CryptFile("crypting", std::ios::binary);
 	if (!InputFile) {
+		InputFile.close();
+		CryptFile.close();
+		system("del /f crypting");
 		std::cout << "Không tìm thấy hoặc không thể truy cập file ";
 		AnimationCryptorObj.DotAnimation(500);
 		std::cin.clear();
 		std::cin.ignore(10000, '\n');
 		goto escape;
 	}
-	TemporyFile.open("process", std::fstream::out);
-	if (!TemporyFile) {
-		std::cout << "Không thể tạo file tạm thời ";
+	if (!CryptFile) {
+		std::cout << "Không thể tạo file tạm thời (Thiếu quyền?) ";
 		AnimationCryptorObj.DotAnimation(500);
 		std::cin.clear();
 		std::cin.ignore(10000, '\n');
@@ -28,52 +38,46 @@ void cryptor::Encrypt(std::string filename, int pin) {
 	}
 	while (InputFile >> std::noskipws >> i) {
 		i = i + pin;
-		TemporyFile << i;
+		TempData += i;
 	}
-	InputFile.close();
-	TemporyFile.close();
-	InputFile.open(filename, std::fstream::out);
-	if (!InputFile) {
-		std::cout << "Không tìm thấy hoặc không thể truy cập file ";
-		AnimationCryptorObj.DotAnimation(500);
-		std::cin.clear();
-		std::cin.ignore(10000, '\n');
-		goto escape;
-	}
-	TemporyFile.open("process", std::fstream::in);
-	if (!TemporyFile) {
-		std::cout << "Không thể tạo file tạm thời ";
-		AnimationCryptorObj.DotAnimation(500);
-		std::cin.clear();
-		std::cin.ignore(10000, '\n');
-		goto escape;
-	}
-	while (TemporyFile >> std::noskipws >> i) {
-		InputFile << i;
-	}
-	InputFile.close();
-	TemporyFile.close();
+	CryptFile << TempData;
 	std::cout << "Tệp đã được mã hoá với mã PIN: " << pin << std::endl;
-	system("del /f process");
+	Sleep(1000);
+	InputFile.close();
+	CryptFile.close();
+	system(("del /f " + OriginalFileName).c_str());
+	system(("ren crypting " + OriginalFileName).c_str());
+	dw2 = GetTickCount64();
+	dw3 = dw2 - dw1;
+	dw3 = dw3 / 1000;
+	std::cout << "Quá trình mã hoá tốn " << dw3 << " giây." << std::endl;
 	system("pause");
 escape:
 	std::cout << "";
 }
 
 void cryptor::Decrypt(std::string filename, int pin) {
+	DWORD dw1;
+	DWORD dw2;
+	double dw3;
 	char i;
-	std::fstream InputFile, TemporyFile;
-	InputFile.open(filename, std::fstream::in);
+	dw1 = GetTickCount64();
+	std::string OriginalFileName = filename;
+	std::string TempData;
+	std::ifstream InputFile(filename, std::ios::binary);
+	std::ofstream DecryptFile("decrypting", std::ios::binary);
 	if (!InputFile) {
+		InputFile.close();
+		DecryptFile.close();
+		system("del /f decrypting");
 		std::cout << "Không tìm thấy hoặc không thể truy cập file ";
 		AnimationCryptorObj.DotAnimation(500);
 		std::cin.clear();
 		std::cin.ignore(10000, '\n');
 		goto escape;
 	}
-	TemporyFile.open("process.txt", std::fstream::out);
-	if (!TemporyFile) {
-		std::cout << "Không thể tạo file tạm thời ";
+	if (!DecryptFile) {
+		std::cout << "Không thể tạo file tạm thời (Thiếu quyền?) ";
 		AnimationCryptorObj.DotAnimation(500);
 		std::cin.clear();
 		std::cin.ignore(10000, '\n');
@@ -81,35 +85,19 @@ void cryptor::Decrypt(std::string filename, int pin) {
 	}
 	while (InputFile >> std::noskipws >> i) {
 		i = i - pin;
-		TemporyFile << i;
+		TempData += i;
 	}
-	InputFile.close();
-	TemporyFile.close();
-	InputFile.open(filename, std::fstream::out);
-	if (!InputFile) {
-		std::cout << "Không tìm thấy hoặc không thể truy cập file ";
-		AnimationCryptorObj.DotAnimation(500);
-		std::cin.clear();
-		std::cin.ignore(10000, '\n');
-		std::cin.clear();
-		std::cin.ignore(10000, '\n');
-		goto escape;
-	}
-	TemporyFile.open("process.txt", std::fstream::in);
-	if (!TemporyFile) {
-		std::cout << "Không thể tạo file tạm thời ";
-		AnimationCryptorObj.DotAnimation(500);
-		std::cin.clear();
-		std::cin.ignore(10000, '\n');
-		goto escape;
-	}
-	while (TemporyFile >> std::noskipws >> i) {
-		InputFile << i;
-	}
-	InputFile.close();
-	TemporyFile.close();
+	DecryptFile << TempData;
 	std::cout << "Tệp đã được giải mã với mã PIN: " << pin << std::endl;
-	system("del /f process.txt");
+	Sleep(1000);
+	InputFile.close();
+	DecryptFile.close();
+	system(("del /f " + OriginalFileName).c_str());
+	system(("ren decrypting " + OriginalFileName).c_str());
+	dw2 = GetTickCount64();
+	dw3 = dw2 - dw1;
+	dw3 = dw3 / 1000;
+	std::cout << "Quá trình giải mã tốn " << dw3 << " giây." << std::endl;
 	system("pause");
 escape:
 	std::cout << "";
